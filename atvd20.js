d090754140a48f61642dfa1ca849e7344e5f727b
Franklin-Chlamtac <89341529+Franklin-Chlamtac@users.noreply.github.com>
2022-07-30 21:20:20 -0300

    POOp

---
+++ atvd20.js
@@ -0,0 +1,15 @@
+class funci{
+    static qntd = 0;
+    static qntdm(){
+        qntd +=1;
+        return qntd;
+    }
+    
+    
+}
+
+console.log(funci.qntd);
+//PROBLEMAS PRA INCREMENTAR... TIRAR DUVIDA EM SALA DE AULA!
+
+
+
